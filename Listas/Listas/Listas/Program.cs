5f313660b4f8836d5b455c21e00426efafc09b88
deenedev <danielfalcaoviktorio@gmail.com>
2024-06-17 19:42:06 -0300

    feat: estudo de listas e tipos genericos

---
+++ Listas/Listas/Listas/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using RestSharp;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var payments = new List<Payment>();
+            payments.Add(new Payment(1, "Monitoro", "98"));
+            payments.Add(new Payment(2, "Cinderelo", "100"));
+            payments.Add(new Payment(3, "Marmito", "500"));
+            payments.Add(new Payment(4, "Esponjo", "321"));
+            payments.Add(new Payment(5, "Pericles", "10"));
+            foreach (var payment in payments)
+            {
+                Console.Write(payment.Id);
+                Console.Write($"\t{payment.Valor}\t");
+                Console.Write(payment.Name);
+                Console.Write("\n");
+            }
+            var found = payments.Where(pay => pay.Id == 3);
+            foreach (var fPayments in found)
+            {
+                Console.WriteLine(fPayments.Id);
+            }
+        }
+    }
+
+    public class Payment
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Valor { get; set; } 
+
+        public Payment(int id, string name, string valor)
+        {
+            Id = id;
+            Name = name;
+            Valor = $"R${valor}";
+        }
+    }
+}
---
+++ TiposGenericos/TiposGenericos/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace TiposGenericos
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person user = new Person();
+            Subscription sub = new Subscription();
+            Payment payMethod = new Payment();
+            var payContext = new PaymentContext<Person, Payment, Subscription>();
+            Person persona = payContext.Save(user);
+        }
+    }
+
+    public class PaymentContext<PE, PA, SU>
+        where PE : Person
+        where PA : Payment
+        where SU : Subscription
+    {
+       public PE Save(PE person)
+        {
+            return person;
+        }
+
+        public PA Save(PA paymentMethod)
+        {
+            return paymentMethod;
+        }
+
+        public SU Save(SU subscription)
+        {
+            return subscription;
+        }
+    }
+
+    public class Person
+    {
+
+    }
+
+    public class Payment
+    {
+
+    }
+
+    public class Subscription
+    {
+
+    }
+}
